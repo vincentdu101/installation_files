982008930f52cb61ca6895ad3dc7f28c533f28c6
Vincent Du <vincentdu101@gmail.com>
2014-04-02 22:37:04 -0700

    Added java tomcat and lamp server installation files

---
+++ java.sh
@@ -0,0 +1,8 @@
+sudo mkdir -p /usr/local/java
+cd ~/Downloads
+sudo cp -r jdk-7u51-linux-x64.tar.gz /usr/local/java
+sudo cp -r jre-7u51-linux-x64.tar.gz /usr/local/java
+cd /usr/local/java
+sudo tar xvzf jdk-7u51-linux-x64.tar.gz
+sudo tar xvzf jre-7u51-linux-x64.tar.gz
+
---
+++ lamp.sh
@@ -0,0 +1,5 @@
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt-get install php5 libapache2-mod-php5
+sudo /etc/init.d/apache2 restart
+php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";''

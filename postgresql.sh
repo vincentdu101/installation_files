aa07fc69181f0d29571edaeb8ac58d0d1dab3dca
Vincent Du <vincentdu101@gmail.com>
2014-04-14 06:42:52 -0700

    Added missing library for ruby-pg

--- postgresql.sh
+++ postgresql.sh
@@ -4,3 +4,4 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 sudo apt-get update
 sudo apt-get install postgresql-9.3 pgadmin3
+sudo apt-get install libpq-dev

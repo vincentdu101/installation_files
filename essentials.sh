45559e0a8ea95c4a5c94bb12b55f52cf23ebfc8a
Vincent Du <OFFICE\vdu@irdev97.office.onescreeninc.com>
2014-07-02 12:24:03 -0700

    Added git coloring for console terminal commands

--- essentials.sh
+++ essentials.sh
@@ -1,1 +1,5 @@
 sudo apt-get install htop
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto

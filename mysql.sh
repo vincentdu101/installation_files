aaa10fa463875e9979323ac1f06225e00551800c
Vince Du <vdu@irdev24.office.onescreeninc.com>
2014-09-16 11:09:58 -0700

    Updated to add sublime plugins, mysql changes, and new version

---
+++ mysql.sh
@@ -0,0 +1,13 @@
+sudo apt-get install mysql-server
+sudo /etc/init.d/mysql stop
+sudo mysqld_safe --skip-grant-tables &
+
+echo "Look inside the mysql.sh and follow the directions that are commented to reset password of root"
+# mysql -u root
+# use mysql;
+# update user set password=PASSWORD("mynewpassword") where User='root';
+# flush privileges;
+# exit
+# sudo /etc/init.d/mysql stop
+# sudo /etc/init.d/mysql start
+# mysql -u root -p

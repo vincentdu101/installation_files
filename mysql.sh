0ab07be1cd8e2adbccd96874f9365d68dc7a6312
Vincent Du <vincentdu101@gmail.com>
2014-09-16 12:14:01 -0700

    Added mysql-workbench and removed duplicate directories

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,8 @@
 sudo apt-get install mysql-server
 sudo /etc/init.d/mysql stop
 sudo mysqld_safe --skip-grant-tables &
+sudo apt-get install mysql-workbench
+
 
 echo "Look inside the mysql.sh and follow the directions that are commented to reset password of root"
 # mysql -u root

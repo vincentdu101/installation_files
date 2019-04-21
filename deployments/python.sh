badd3e8fe7cbca3c584f73a6aef30890d1d6003e
vincentdu101 <vincentdu101@gmail.com>
2019-04-21 10:42:50 -0700

    fixed path issues with installing requirements.txt

--- deployments/python.sh
+++ deployments/python.sh
@@ -41,10 +41,10 @@ function clone_app_repository() {
     if [ -d ~/wildfire-server]; then
         sudo rm -rf ~/wildfire-server
         git clone https://github.com/vincentdu101/Wildfire-Analyzer-Predictor-System.git ~/wildfire-server
-        cd ~/wildfire-server/
+        cd ~/wildfire-server/server/
     else
         git clone https://github.com/vincentdu101/Wildfire-Analyzer-Predictor-System.git ~/wildfire-server
-        cd ~/wildfire-server/
+        cd ~/wildfire-server/server/
     fi
 }
 

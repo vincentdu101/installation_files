712663f7f31e9b3d29ac61296d74ba3e520f0cce
vincentdu101 <vincentdu101@gmail.com>
2019-05-03 21:09:43 -0700

    adjusted wildfire-server api redirect

--- deployments/shape_runner.sh
+++ deployments/shape_runner.sh
@@ -4,6 +4,7 @@ function setup_node_environment() {
     # download nvm
     echo Downloading nvm
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    source ~/.bashrc
 
     # install the latest node version
     echo Install the latest node version
